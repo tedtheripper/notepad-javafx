c11eff390078c758729bb496d7d9bd1e85366497
tedtheripper <lemon.juice0731@gmail.com>
2021-01-03 21:28:19 +0100

    added main page content and set minimum w, h for the window

--- hellofx/src/main/java/org/openjfx/App.java
+++ hellofx/src/main/java/org/openjfx/App.java
@@ -5,12 +5,15 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -24,20 +27,26 @@ import java.util.List;
 public class App extends Application {
 
     private static Scene scene;
-    private MenuBarController menuBarController;
-
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         //scene = new Scene(loadFXML("primary"), 640, 480);
         primaryStage.setTitle("NoteCrash");
-
-        menuBarController = new MenuBarController();
+        primaryStage.setMinWidth(500);
+        primaryStage.setMinHeight(500);
+        VBox vBox = new VBox();
 
         Menu menuFile = new Menu("File");
         List<String> menuNames = Arrays.asList("New", "Open", "Save", "SaveAs", "Exit");
         List<MenuItem> menuItems = new ArrayList<>();
-        EventHandler<ActionEvent> action = act();
+        EventHandler<ActionEvent> action = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                MenuItem mItem = (MenuItem) actionEvent.getSource();
+                String name = mItem.getText();
+                menuBarAction(name);
+            }
+        };
         for(var name : menuNames){
             var menuItem = new MenuItem(name);
             menuItem.setOnAction(action);
@@ -47,57 +56,88 @@ public class App extends Application {
 
         Menu menuSearch = new Menu("Search");
         MenuItem menuItemSearch = new MenuItem("search");
+        menuItemSearch.setOnAction(action);
         menuSearch.getItems().add(menuItemSearch);
         MenuBar menuBar = new MenuBar();
 
-        menuBar.getMenus().add(menuFile);
-        menuBar.getMenus().add(menuSearch);
-        VBox vBox = new VBox(menuBar);
-        Scene scene = new Scene(vBox, 960, 600);
+        menuBar.getMenus().addAll(menuFile, menuSearch);
+        vBox.getChildren().add(menuBar);
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setHgap(5);
+        grid.setVgap(5);
+        Label labelAuthor = new Label("Author: ");
+        TextField textFieldAuthor = new TextField();
+        GridPane.setConstraints(labelAuthor, 0, 0);
+        GridPane.setConstraints(textFieldAuthor, 1, 0);
+        grid.getChildren().addAll(labelAuthor, textFieldAuthor);
+        Label labelCategory = new Label("Category: ");
+        TextField textFieldCategory = new TextField();
+        GridPane.setConstraints(labelCategory, 0, 1);
+        GridPane.setConstraints(textFieldCategory, 1, 1);
+        grid.getChildren().addAll(labelCategory, textFieldCategory);
+
+        GridPane grid2 = new GridPane();
+        grid2.setPadding(new Insets(10, 10, 10, 10));
+        TextArea textArea = new TextArea();
+        //TextField textFieldArea = new TextField();
+        GridPane.setConstraints(textArea, 0, 2);
+        textArea.setPrefSize(500, 300);
+        grid2.getChildren().add(textArea);
+
+        VBox vb = new VBox();
+        vb.getChildren().addAll(grid, grid2);
+        BorderPane root = new BorderPane();
+        root.setTop(vBox);
+        root.setCenter(vb);
+
+        Scene scene = new Scene(root, 960, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private EventHandler<ActionEvent> act() {
-        return new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                MenuItem mItem = (MenuItem) actionEvent.getSource();
-                String name = mItem.getText();
-                switch(name){
-                    case "New":
-                        System.out.println(name + "pressed");
-                        menuBarController.actionNew();
-                        break;
-                    case "Load":
-                        System.out.println(name + "pressed");
-                        menuBarController.actionLoad();
-                        break;
-                    case "Save":
-                        System.out.println(name + "pressed");
-                        menuBarController.actionSave();
-                        break;
-                    case "SaveAs":
-                        System.out.println(name + "pressed");
-                        menuBarController.actionSaveAs();
-                        break;
-                    case "Exit":
-                        System.out.println(name + "pressed");
-                        menuBarController.actionExit();
-                        break;
-                }
-            }
-        };
+    private void menuBarAction(String name) {
+        switch(name){
+            case "New":
+                menuBarActionNew();
+                break;
+            case "Load":
+                menuBarActionLoad();
+                break;
+            case "Save":
+                menuBarActionSave();
+                break;
+            case "SaveAs":
+                menuBarActionSaveAs();
+                break;
+            case "Exit":
+                menuBarActionExit();
+                break;
+            case "search":
+                menuBarActionSearch();
+                break;
+        }
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+    private void menuBarActionSearch() {
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    private void menuBarActionExit() {
     }
+
+    private void menuBarActionSaveAs() {
+    }
+
+    private void menuBarActionSave() {
+    }
+
+    private void menuBarActionLoad() {
+    }
+
+    private void menuBarActionNew() {
+    }
+
 
     public static void main(String[] args) {
         launch();
